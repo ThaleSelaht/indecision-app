4d9f408e9c6ece144e133caeb432c62988ab8bc4
Thales <thalespizza@gmail.com>
2018-06-18 18:30:17 -0300

    Adicionando title e subtitle

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,12 +2,65 @@
 
 console.log('App.js is running!');
 
-// JSX - JavaScript XML
+var app = {
+  title: 'Indecision App',
+  subtitle: 'Put your life in the hands of a computer'
+};
+
 var template = React.createElement(
-  'h1',
+  'div',
   null,
-  'Indecision App'
+  React.createElement(
+    'h1',
+    null,
+    app.title
+  ),
+  React.createElement(
+    'p',
+    null,
+    app.subtitle
+  ),
+  React.createElement(
+    'ol',
+    null,
+    React.createElement(
+      'li',
+      null,
+      'Item One'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'Item Two'
+    )
+  )
+);
+var user = {
+  name: 'Thales',
+  age: 28,
+  location: 'Cubatão'
+};
+
+var templateTwo = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    user.name
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Age:',
+    user.age
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Location:',
+    user.location
+  )
 );
 var appRoot = document.getElementById('app');
-
 ReactDOM.render(template, appRoot);
